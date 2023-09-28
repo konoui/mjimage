f5e2a2b752c74f020a653df177c23d0e0981992f
konoui <konoui.dev@gmail.com>
2023-09-28 22:11:37 +0900

    add table poc

--- scripts/pngtosvg.sh
+++ scripts/pngtosvg.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-IMAGE_DIR="pkg/mjimage/images"
+SRC_IMAGE_DIR="./static/png/"
+DST_IMAGE_DIR="./static/svg"
 PNG_FILES="*.png"
 # go install github.com/xyproto/png2svg/cmd/png2svg@latest
 while read -r f; do
 
   input_path=$f
   target_name=$(basename $f | cut -f 1 -d ".").svg
-  png2svg -v -q -o $IMAGE_DIR/svg/$target_name $input_path
+  png2svg -v -q -o $DST_IMAGE_DIR/$target_name $input_path
 
-done < <(find $IMAGE_DIR $PNG_FILES -mindepth 1 -maxdepth 1 -type f)
+done < <(find $SRC_IMAGE_DIR $PNG_FILES -mindepth 1 -maxdepth 1 -type f)
