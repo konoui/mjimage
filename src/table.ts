8c6e3948213c84e9349de5ef44f1c8c38d36904e
konoui <konoui.dev@gmail.com>
2023-10-07 21:18:46 +0900

    update draw api to avoid add global element

--- src/table.ts
+++ src/table.ts
@@ -1,6 +1,6 @@
 import { Pai, Operator, Parser, Kind } from "./parser";
 import { ImageHelper, createHand } from "./image";
-import { Svg, SVG, Element, Text } from "@svgdotjs/svg.js";
+import { Svg, SVG, Element, Text, G, Rect, Image } from "@svgdotjs/svg.js";
 import assert from "assert";
 
 const splitTiles = (input: Pai[]) => {
@@ -14,13 +14,12 @@ const splitTiles = (input: Pai[]) => {
 };
 
 const simpleRotate = (
-  draw: Svg,
   e: Element,
   width: number,
   height: number,
   degree: 0 | 90 | 180 | 270 = 90
 ) => {
-  const g = draw.group().add(e);
+  const g = new G().add(e);
   if (degree == 90) {
     const translatedX = 0;
     const translatedY = 0 - height;
@@ -56,13 +55,13 @@ let contextFunc = (s: string, font: string | null = null) => {
   };
 };
 
-const createBoard = (draw: Svg, helper: ImageHelper, width: number) => {
+const createBoard = (helper: ImageHelper, width: number) => {
   // FIXME merge drawRect and consider helper.scale
   const font = { family: "メイリオ,ＭＳ Ｐゴシック", size: 15 };
   const fontString = `${font.size}px ${font.family}`;
   const [textWidth, textHeight] = contextFunc("東", fontString)();
 
-  const g = draw.group();
+  const g = new G();
 
   const num100 = 1;
   const num1000 = 1;
@@ -73,7 +72,7 @@ const createBoard = (draw: Svg, helper: ImageHelper, width: number) => {
   let roundHeight = textHeight;
   const roundX = (stickWidth + helper.paiWidth - roundWidth) / 2;
 
-  const roundText = draw
+  const roundText = new Text()
     .text("東１局")
     .font({ font: 15, family: font.family })
     .move(roundX, 0);
@@ -82,16 +81,15 @@ const createBoard = (draw: Svg, helper: ImageHelper, width: number) => {
   roundHeight += 10; // margin
 
   const stickGroupHeight = helper.paiHeight;
-  const stickGroup = draw
-    .group()
+  const stickGroup = new G()
     .size(stickWidth, stickGroupHeight)
     .translate(0, roundHeight);
 
-  const stick1000 = draw
-    .image(helper.makeImageHref("stick1000.svg"))
+  const stick1000 = new Image()
+    .load(helper.makeImageHref("stick1000.svg"))
     .size(stickWidth, stickHeight)
     .move(0, 0);
-  const text1000 = draw
+  const text1000 = new Text()
     .text(num1000.toString())
     // margin -3
     .move(stickWidth, -3)
@@ -99,11 +97,11 @@ const createBoard = (draw: Svg, helper: ImageHelper, width: number) => {
   stickGroup.add(stick1000);
   stickGroup.add(text1000);
 
-  const stick100 = draw
-    .image(helper.makeImageHref("stick100.svg"))
+  const stick100 = new Image()
+    .load(helper.makeImageHref("stick100.svg"))
     .size(stickWidth, stickHeight)
     .move(0, stickHeight + stickHeight);
-  const text100 = draw
+  const text100 = new Text()
     .text(num100.toString())
     .move(stickWidth, stickHeight - 3 + stickHeight)
     .font({ size: 13, family: font.family });
@@ -111,7 +109,7 @@ const createBoard = (draw: Svg, helper: ImageHelper, width: number) => {
   stickGroup.add(text100);
 
   const doraImg = helper
-    .createImage(draw, new Pai(Kind.M, 2), 0, 0)
+    .createImage(new Pai(Kind.M, 2), 0, 0)
     .move(stickWidth + textWidth, 0);
   stickGroup.add(doraImg);
 
@@ -125,7 +123,7 @@ const createBoard = (draw: Svg, helper: ImageHelper, width: number) => {
   };
 };
 
-const createHands = (draw: Svg, helper: ImageHelper) => {
+const createHands = (helper: ImageHelper) => {
   const input = "2s, -1111p, -1111s, -1111m, -2222m, t3s";
   // max case: "2s, -1111p, 1111s, -1111m, -2222m t3s"
   const sizeWidth =
@@ -137,27 +135,27 @@ const createHands = (draw: Svg, helper: ImageHelper) => {
     helper.paiWidth * 2; // additional margin
   const sizeHeight = sizeWidth;
 
-  const g = draw.group().size(sizeWidth, sizeHeight);
+  const g = new G().size(sizeWidth, sizeHeight);
 
   const blocks = new Parser(input).parse();
-  const z1e = createHand(draw, blocks, helper);
+  const z1e = createHand(blocks, helper);
 
-  const z2e = createHand(draw, blocks, helper);
-  const z3e = createHand(draw, blocks, helper);
-  const z4e = createHand(draw, blocks, helper);
-  const z1g = simpleRotate(draw, z1e.e, z1e.width, z1e.height, 0).translate(
+  const z2e = createHand(blocks, helper);
+  const z3e = createHand(blocks, helper);
+  const z4e = createHand(blocks, helper);
+  const z1g = simpleRotate(z1e.e, z1e.width, z1e.height, 0).translate(
     (sizeWidth - z1e.width) / 2,
     sizeHeight - z1e.height
   );
-  const z2g = simpleRotate(draw, z2e.e, z2e.width, z2e.height, 270).translate(
+  const z2g = simpleRotate(z2e.e, z2e.width, z2e.height, 270).translate(
     sizeWidth - z2e.height,
     (sizeWidth - z2e.width) / 2
   );
-  const z3g = simpleRotate(draw, z3e.e, z3e.width, z3e.height, 180).translate(
+  const z3g = simpleRotate(z3e.e, z3e.width, z3e.height, 180).translate(
     (sizeWidth - z3e.width) / 2,
     0
   );
-  const z4g = simpleRotate(draw, z4e.e, z4e.width, z4e.height, 90).translate(
+  const z4g = simpleRotate(z4e.e, z4e.width, z4e.height, 90).translate(
     0,
     (sizeWidth - z4e.width) / 2
   );
@@ -170,42 +168,42 @@ const createHands = (draw: Svg, helper: ImageHelper) => {
   return { e: g, width: sizeWidth, height: sizeHeight };
 };
 
-const drawRect = (draw: Svg, helper: ImageHelper, width: number) => {
-  const g = draw.group();
-  const rect = draw
-    .rect(width, width)
+const drawRect = (helper: ImageHelper, width: number) => {
+  const g = new G();
+  const rect = new Rect()
+    .size(width, width)
     .attr({ fill: "none", stroke: "#000000" });
   g.add(rect);
 
   const font = { family: "メイリオ,ＭＳ Ｐゴシック", size: 15 };
   const fontString = `${font.size}px ${font.family}`;
   let [textWidth, textHeight] = contextFunc("東", fontString)();
-  const boardRect = createBoard(draw, helper, width / 1.1);
+  const boardRect = createBoard(helper, width / 1.1);
   boardRect.e.translate(
     width / 2 - boardRect.width / 2,
     width / 2 - boardRect.height / 2
   );
   g.add(boardRect.e);
 
-  const w1Text = draw
+  const w1Text = new Text()
     .text("東")
     .font(font)
     .move(width / 2 - textWidth / 2, width - textHeight);
   g.add(w1Text);
 
-  const w3Text = draw
+  const w3Text = new Text()
     .text("西")
     .font(font)
     .move(width / 2 - textWidth / 2, 0);
   g.add(w3Text);
 
-  const w2Text = draw
+  const w2Text = new Text()
     .text("南")
     .font(font)
     .move(width - textWidth, width / 2 - textHeight / 2);
   g.add(w2Text);
 
-  const w4Text = draw
+  const w4Text = new Text()
     .text("北")
     .font(font)
     .move(0, width / 2 - textHeight / 2);
@@ -214,46 +212,40 @@ const drawRect = (draw: Svg, helper: ImageHelper, width: number) => {
   return g;
 };
 
-const drawDiscards = (draw: Svg, helper: ImageHelper, p: any) => {
+const drawDiscards = (helper: ImageHelper, p: any) => {
   const discardWidth = helper.paiWidth * 5 + helper.paiHeight * 1; // 11111-1
   const discardHeight = helper.paiHeight * 4;
 
   const sizeWidth = helper.paiWidth * 18;
   const sizeHeight = helper.paiWidth * 18;
 
-  const g = draw.group().size(sizeWidth, sizeHeight);
+  const g = new G().size(sizeWidth, sizeHeight);
 
   const centerX = sizeWidth / 2 - discardWidth / 2;
   const centerY = sizeHeight / 2 - discardWidth / 2;
-  const rect = drawRect(draw, helper, discardWidth).translate(centerX, centerY);
+  const rect = drawRect(helper, discardWidth).translate(centerX, centerY);
   g.add(rect);
 
-  let z1 = handleDiscard(draw, p, helper);
-  z1 = simpleRotate(draw, z1, discardWidth, discardHeight, 0).translate(
+  let z1 = handleDiscard(p, helper);
+  z1 = simpleRotate(z1, discardWidth, discardHeight, 0).translate(
     centerX,
     sizeHeight - discardHeight
   );
   g.add(z1);
 
-  let z2 = handleDiscard(draw, p, helper);
-  z2 = simpleRotate(draw, z2, discardWidth, discardHeight, 270).translate(
+  let z2 = handleDiscard(p, helper);
+  z2 = simpleRotate(z2, discardWidth, discardHeight, 270).translate(
     sizeWidth - discardHeight,
     centerY
   );
   g.add(z2);
 
-  let z3 = handleDiscard(draw, p, helper);
-  z3 = simpleRotate(draw, z3, discardWidth, discardHeight, 180).translate(
-    centerX,
-    0
-  );
+  let z3 = handleDiscard(p, helper);
+  z3 = simpleRotate(z3, discardWidth, discardHeight, 180).translate(centerX, 0);
   g.add(z3);
 
-  let z4 = handleDiscard(draw, p, helper);
-  z4 = simpleRotate(draw, z4, discardWidth, discardHeight, 90).translate(
-    0,
-    centerY
-  );
+  let z4 = handleDiscard(p, helper);
+  z4 = simpleRotate(z4, discardWidth, discardHeight, 90).translate(0, centerY);
   g.add(z4);
   return { e: g, width: sizeWidth, height: sizeHeight };
 };
@@ -265,10 +257,10 @@ export const handle = () => {
 
   const draw = SVG().size(1000, 1000);
 
-  const hg = createHands(draw, helper);
+  const hg = createHands(helper);
   draw.add(hg.e);
 
-  const dg = drawDiscards(draw, helper, p);
+  const dg = drawDiscards(helper, p);
   dg.e.translate((hg.width - dg.width) / 2, (hg.height - dg.height) / 2);
   draw.add(dg.e);
 
@@ -276,8 +268,8 @@ export const handle = () => {
   draw.addTo("#container");
 };
 
-const handleDiscard = (draw: Svg, pp: Pai[], helper: ImageHelper) => {
-  const g = draw.group();
+const handleDiscard = (pp: Pai[], helper: ImageHelper) => {
+  const g = new G();
   const chunks = splitTiles(pp);
 
   for (let i = 0; i < chunks.length; i++) {
@@ -286,12 +278,12 @@ const handleDiscard = (draw: Svg, pp: Pai[], helper: ImageHelper) => {
     let posX = 0;
     for (let p of chunk) {
       if (p.op == Operator.Horizontal) {
-        const img = helper.createRotate90Image(draw, p, posX, posY);
+        const img = helper.createRotate90Image(p, posX, posY);
         g.add(img);
         posX += helper.paiHeight;
         continue;
       }
-      const img = helper.createImage(draw, p, posX, posY);
+      const img = helper.createImage(p, posX, posY);
       g.add(img);
       posX += helper.paiWidth;
     }
