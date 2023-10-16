4d0f1bfeefc59661d5efec001a22a118bf6ea9e7
konoui <konoui.dev@gmail.com>
2023-10-16 21:03:55 +0900

    add test

--- src/table.test.ts
+++ src/table.test.ts
@@ -16,27 +16,28 @@ registerWindow(window, document);
 
 config.setFontDir("./node_modules/svgdom/fonts/");
 
+// FIXME using node canvas
+const fontCtx: FontContext = {
+  font: { family: FONT_FAMILY, size: 16 },
+  textWidth: 16.040000915527344,
+  textHeight: 16.040000915527344,
+  numWidth: 11.84,
+  numHeight: 11.84,
+};
+
+const helper = new ImageHelper({
+  imageHostPath: "http://localhost:1234/svg/",
+  scale: 0.4,
+});
+
 const update = false;
 
-test("table-svg1", () => {
+test("max-table-size", () => {
   const sampleDiscard = "123456789s12-3456789m1234p";
   const p = new Parser(sampleDiscard).parseInput();
 
   const sampleHand = "2s, -1111p, -1111s, -1111m, -2222m, t3s";
   const blocks = new Parser(sampleHand).parse();
-
-  const helper = new ImageHelper({
-    imageHostPath: "http://localhost:1234/svg/",
-    scale: 0.4,
-  });
-  // FIXME using node canvas
-  const fontCtx: FontContext = {
-    font: { family: FONT_FAMILY, size: 16 },
-    textWidth: 16.040000915527344,
-    textHeight: 16.040000915527344,
-    numWidth: 11.84,
-    numHeight: 11.84,
-  };
 
   const hands: HandsInput = {
     front: blocks,
@@ -72,8 +73,61 @@ test("table-svg1", () => {
 
   draw.add(g.e);
   const got = draw.svg();
-  const gotPath = "testdata/table-svg1.svg";
-  if (update) fs.writeFileSync("testdata/table-svg1.svg", got);
-  const want = fs.readFileSync(gotPath);
+  const want = loadTestData("table-svg1.svg", update, got);
   expect(want.toString()).toBe(got);
 });
+
+test("dynamic-hands-size", () => {
+  const sampleDiscard = "1p";
+  const p = new Parser(sampleDiscard).parseInput();
+
+  const sampleHand = "123456789s1234m";
+  const blocks = new Parser(sampleHand).parse();
+  const hands: HandsInput = {
+    front: new Parser("123456789s1234m, t3s").parse(),
+    right: blocks,
+    opposite: blocks,
+    left: blocks,
+  };
+  const discards: DiscardsInput = {
+    front: p,
+    right: p,
+    opposite: p,
+    left: p,
+  };
+  const scoreBoard: ScoreBoardInput = {
+    round: "南４局",
+    scores: {
+      front: 100,
+      right: 200,
+      opposite: 25000,
+      left: 9000,
+    },
+    frontPlace: "西",
+    sticks: {
+      reach: 1,
+      dead: 3,
+    },
+    doras: [new Tile(KIND.M, 3)],
+  };
+
+  const draw = SVG();
+
+  const g = createTable(helper, fontCtx, hands, discards, scoreBoard);
+
+  draw.add(g.e);
+  const got = draw.svg();
+  const want = loadTestData("table-svg2.svg", update, got);
+  expect(want.toString()).toBe(got);
+});
+
+const loadTestData = (
+  filename: string,
+  update: boolean = false,
+  data: string = ""
+) => {
+  const gotPath = `testdata/${filename}`;
+  if (update) fs.writeFileSync(gotPath, data);
+  const want = fs.readFileSync(gotPath);
+  return want;
+};
