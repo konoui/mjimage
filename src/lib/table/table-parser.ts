d5e3a1096c45a5f4a856a91dbc8d56ff3a8abdf6
konoui <konoui.dev@gmail.com>
2024-09-27 21:56:39 +0900

    update remove js-yaml

--- src/lib/table/table-parser.ts
+++ src/lib/table/table-parser.ts
@@ -1,4 +1,3 @@
-import { load } from "js-yaml";
 import {
   string,
   number,
@@ -10,6 +9,7 @@ import {
   safeParse,
   InferOutput,
   strictObject,
+  InferInput,
 } from "valibot";
 import { Tile, Parser, Block } from "../core/parser";
 import {
@@ -41,18 +41,41 @@ const windInputsSchema = strictObject({
 });
 
 // boardInputSchema の定義
-const boardInputSchema = strictObject({
-  round: optional(picklist(Object.keys(ROUND_MAP) as TableRound[]), ROUND.E1),
-  sticks: optional(
-    strictObject({
-      reach: optional(pipe(number(), minValue(0, ""), maxValue(9, "")), 0),
-      dead: optional(pipe(number(), minValue(0, ""), maxValue(9, "")), 0),
-    }),
-    { reach: 0, dead: 0 }
-  ),
-  doras: optional(string(), WIND.S),
-  front: optional(picklist(Object.keys(WIND_MAP) as TableWind[]), WIND.E),
-});
+
+const defaultBoard = {
+  round: ROUND.E1,
+  sticks: { reach: 0, dead: 0 },
+  doras: WIND.S,
+  front: WIND.E,
+};
+
+const boardInputSchema = optional(
+  strictObject({
+    round: optional(
+      picklist(Object.keys(ROUND_MAP) as TableRound[]),
+      defaultBoard.round
+    ),
+    sticks: optional(
+      strictObject({
+        reach: optional(
+          pipe(number(), minValue(0, ""), maxValue(9, "")),
+          defaultBoard.sticks.reach
+        ),
+        dead: optional(
+          pipe(number(), minValue(0, ""), maxValue(9, "")),
+          defaultBoard.sticks.dead
+        ),
+      }),
+      { reach: 0, dead: 0 }
+    ),
+    doras: optional(string(), defaultBoard.doras),
+    front: optional(
+      picklist(Object.keys(WIND_MAP) as TableWind[]),
+      defaultBoard.front
+    ),
+  }),
+  defaultBoard
+);
 
 // tableInputSchema の定義
 const tableInputSchema = strictObject({
@@ -61,9 +84,10 @@ const tableInputSchema = strictObject({
 });
 
 // 型定義
-type WindInput = InferOutput<typeof windInputSchema>;
-type WindInputs = InferOutput<typeof windInputsSchema>;
-type BoardInput = InferOutput<typeof boardInputSchema>;
+type RawWindInput = InferInput<typeof windInputSchema>;
+type RawWindInputs = InferInput<typeof windInputsSchema>;
+type RawBoardInput = InferInput<typeof boardInputSchema>;
+type RawTableInput = InferInput<typeof tableInputSchema>;
 
 export type TableInput = InferOutput<typeof tableInputSchema>;
 
@@ -107,14 +131,103 @@ export const parse = (s: string) => {
 };
 
 export const parseTableInput = (s: string) => {
-  const rawInput = load(s) as { table: TableInput };
+  const rawInput = parseStringInput(s);
 
-  const ret = safeParse(tableInputSchema, rawInput.table);
+  const ret = safeParse(tableInputSchema, rawInput);
   if (!ret.success) {
     throw ret.issues;
   }
   return ret.output;
 };
+
+// ====
+
+const parseStringInput = (input: string): RawTableInput => {
+  const lines = input
+    .split("\n")
+    .map((line) => line.trim())
+    .filter((line) => line != "");
+  if (lines.length == 0) throw new Error("empty input");
+  const tableLabel = lines.shift()!;
+  if (!tableLabel.startsWith("table"))
+    throw new Error(`input does not start with table: ${tableLabel}`);
+
+  const i: RawTableInput = {};
+
+  let labels = [WIND.E, WIND.S, WIND.W, WIND.N, "board"];
+  for (;;) {
+    const line = lines.shift();
+    if (line == undefined) break;
+
+    const label = labels.find((l) => line.startsWith(l))!;
+    if (label == null) throw new Error(`encountered unexpected line ${line}`);
+
+    // update
+    labels = labels.filter((l) => !line.startsWith(l));
+    if (label == "board") {
+      const [bi, count] = handleBoard([...lines]);
+      i.board = bi;
+      for (let i = 0; i < count; i++) lines.shift();
+    } else {
+      const wind = label as Wind;
+      const [wi, count] = handleWind([...lines]);
+      i[wind] = wi;
+      for (let i = 0; i < count; i++) lines.shift();
+    }
+  }
+  return i;
+};
+
+const trim = (s: string, label: string) => {
+  return s.replace(label, "").replace(":", "").trim();
+};
+
+const handleWind = (lines: string[]) => {
+  let r: RawWindInput = {};
+  let i = 0;
+  for (; i < lines.length; i++) {
+    const line = lines[i];
+    if (line.startsWith("hand")) r.hand = trim(line, "hand");
+    else if (line.startsWith("discard")) r.discard = trim(line, "discard");
+    else if (line.startsWith("score")) r.score = Number(trim(line, "score"));
+    else break;
+  }
+  return [r, i] as const;
+};
+
+const handleBoard = (lines: string[]) => {
+  let r: RawBoardInput = {};
+
+  let i = 0;
+  for (; i < lines.length; i++) {
+    const line = lines[i];
+    if (line.startsWith("doras")) {
+      r.doras = trim(line, "doras");
+    } else if (line.startsWith("round")) {
+      r.round = trim(line, "round") as Round;
+    } else if (line.startsWith("front")) {
+      r.front = trim(line, "front") as Wind;
+    } else if (line.startsWith("sticks")) {
+      r.sticks = {};
+      const next = lines[i + 1] ?? "";
+      const nextNext = lines[i + 2] ?? "";
+      if (next.startsWith("reach"))
+        r.sticks.reach = Number(trim(next, "reach"));
+      if (next.startsWith("dead")) r.sticks.dead = Number(trim(next, "dead"));
+      if (nextNext.startsWith("reach"))
+        r.sticks.reach = Number(trim(nextNext, "reach"));
+      if (nextNext.startsWith("dead"))
+        r.sticks.dead = Number(trim(nextNext, "dead"));
+      if (r.sticks.dead != null) i++;
+      if (r.sticks.reach != null) i++;
+    } else {
+      break;
+    }
+  }
+  return [r, i] as const;
+};
+
+// ====
 
 export const convertInput = (i: TableInput) => {
   const frontPlace = i.board.front;
