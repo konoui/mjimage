caaa572bc8bb9dc70dac1b7a402e8f8d18c26059
konoui <konoui.dev@gmail.com>
2025-02-17 14:15:40 +0900

    update sprite test

--- src/lib/__tests__/svg-tile.test.ts
+++ src/lib/__tests__/svg-tile.test.ts
@@ -7,23 +7,23 @@ const update = false;
 const spritePath = "browser-mjimage/static/svg/tiles.svg";
 
 describe("use", () => {
-  // test("import existing", () => {
-  //   const img = fs.readFileSync(spritePath).toString();
-  //   const draw = SVG().importSymbol(img);
-  //   const use = new Use().use("m1");
-  //   draw.add(use);
-  //   const got = draw.svg();
-  //   const want = loadTestData("use.1m.svg", got, update);
-  //   expect(want.toString()).toBe(got);
-  // });
-  // test("remove unused tile", () => {
-  //   const img = fs.readFileSync(spritePath).toString();
-  //   const draw = SVG().importSymbol(img);
-  //   const use = new Use().use("m1");
-  //   draw.add(use);
-  //   optimizeSVG(draw);
-  //   const got = draw.svg();
-  //   const want = loadTestData("use.filtered-m1.svg", got, update);
-  //   expect(want.toString()).toBe(got);
-  // });
+  test("import existing", () => {
+    const img = fs.readFileSync(spritePath).toString();
+    const draw = SVG().importSymbol(img);
+    const use = new Use().use("m1");
+    draw.add(use);
+    const got = draw.svg();
+    const want = loadTestData("use.1m.svg", got, update);
+    expect(want.toString()).toBe(got);
+  });
+  test("remove unused tile", () => {
+    const img = fs.readFileSync(spritePath).toString();
+    const draw = SVG().importSymbol(img);
+    const use = new Use().use("m1");
+    draw.add(use);
+    optimizeSVG(draw);
+    const got = draw.svg();
+    const want = loadTestData("use.filtered-m1.svg", got, update);
+    expect(want.toString()).toBe(got);
+  });
 });
